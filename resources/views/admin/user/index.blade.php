e4db137258d7036528fb94c0c33da3b31d3bd98e
Md Jahidul Islam <jahidcse181@gmail.com>
2025-09-26 21:53:07 +0600

    thfrty

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,65 +1,64 @@
 @extends('admin.adminLayout.master')
 @section('meta')
-    <title>{{ isset($title)?$title:'Users' }} - {{ @siteInfo()->company_name }}</title>
+<title>{{ isset($title)?$title:'Users' }} - {{ @siteInfo()->company_name }}</title>
 @endsection
 
 @section('style')
-    <link rel="stylesheet" href="{{ asset('css/dataTable.css') }}">
+<link rel="stylesheet" href="{{ asset('css/dataTable.css') }}">
 @endsection
 
 @section('main')
 
-<div class=" card row layout-top-spacing">
-    <div class="row">
-        <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-            <div class="statbox widget box box-shadow">
-                <div class="widget-content widget-content-area">
 
-                    <table id="user" class="table dt-table-hover" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>Image</th>
-                                <th>Name</th>
-                                <th>phone</th>
-                                <th>country</th>
-                                <th>balance</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($lists as $list)
-                            @php
-                            if (preg_match('/^data:image\/(\w+);base64,/', @$list->image)) {
-                            $user_image = @$list->image;
-                            } else {
-                            $user_image = asset(@$list->image);
-                            }
-                            @endphp
-                            <tr>
+<div class="row" style="overflow: scroll;">
+    <div class="col-xl-12 col-lg-12 col-sm-12 layout-spacing">
+        <div class="statbox widget box box-shadow">
+            <div class="widget-content widget-content-area">
 
-                                <td><img src="{{ $user_image }}" style="    width: 50px;height: 50px;"></td>
-                                <td>{!! @$list->name !!}</td>
-                                <td>{{ @$list->phone }} </td>
-                                <td>{{ @$list->country->name }} </td>
-                                <td>{{ currency(@$list->balance) }} </td>
-                                <td>{!! @$list->status() !!}</td>
-                                <td class="text-center">
-                                    <a class="btn btn-small btn-primary mb-2" href="{{ route('admin.users.show', $list->id) }}">
-                                        <i class="fa fa-eye"></i> View More
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                <table id="user" class="table dt-table-hover" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>phone</th>
+                            <th>country</th>
+                            <th>balance</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($lists as $list)
+                        @php
+                        if (preg_match('/^data:image\/(\w+);base64,/', @$list->image)) {
+                        $user_image = @$list->image;
+                        } else {
+                        $user_image = asset(@$list->image);
+                        }
+                        @endphp
+                        <tr>
+
+                            <td><img src="{{ $user_image }}" style="    width: 50px;height: 50px;"></td>
+                            <td>{!! @$list->name !!}</td>
+                            <td>{{ @$list->phone }} </td>
+                            <td>{{ @$list->country->name }} </td>
+                            <td>{{ currency(@$list->balance) }} </td>
+                            <td>{!! @$list->status() !!}</td>
+                            <td class="text-center">
+                                <a class="btn btn-small btn-primary mb-2" href="{{ route('admin.users.show', $list->id) }}">
+                                    <i class="fa fa-eye"></i> View More
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
             </div>
         </div>
-
     </div>
 </div>
-<div class="mt-2 mb-5" >
+<div class="mt-2 mb-5">
     {{ $lists->links('pagination::bootstrap-5') }}
 </div>
 
