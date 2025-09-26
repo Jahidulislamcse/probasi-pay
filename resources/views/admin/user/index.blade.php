0f044d98439b7027b5c4ecfc908c4b30d40558a0
Md Jahidul Islam <jahidcse181@gmail.com>
2025-09-26 21:37:56 +0600

    user table updated

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,75 +1,114 @@
 @extends('admin.adminLayout.master')
+
 @section('meta')
-
-<title>{{ isset($title)?$title:'welcome' }} - {{ env('APP_NAME') }}</title>
-
+<title>{{ isset($title) ? $title : 'Welcome' }} - {{ env('APP_NAME') }}</title>
 @endsection
+
 @section('style')
+<!-- Bootstrap 5 DataTables CSS -->
+<link href="https://cdn.datatables.net/1.13.8/css/dataTables.bootstrap5.min.css" rel="stylesheet">
 
+<style>
+    .card {
+        border: none;
+        border-radius: 0.75rem;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.06);
+    }
+    .card-header {
+        border-bottom: none;
+        border-radius: 0.75rem 0.75rem 0 0;
+        background: #f8f9fa;
+    }
+    .card-header h5 {
+        margin: 0;
+        font-weight: 600;
+    }
+    table.dataTable thead th {
+        background-color: #f8f9fa;
+        color: #212529;
+        font-weight: 600;
+        border-bottom: 2px solid #dee2e6;
+    }
+    table.dataTable tbody tr:hover {
+        background-color: #f1f3f5;
+    }
+    .user-img {
+        width: 45px;
+        height: 45px;
+        object-fit: cover;
+        border-radius: 50%;
+        border: 2px solid #dee2e6;
+    }
+</style>
 @endsection
+
 @section('main')
-
-<div class=" card row layout-top-spacing">
-    <div class="row">
-        <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-            <div class="statbox widget box box-shadow">
-                <div class="widget-content widget-content-area">
-
-                    <table id="user" class="table dt-table-hover" style="width:100%">
+<div class="row layout-top-spacing">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header text-white d-flex justify-content-between align-items-center">
+                <h5>{{ $title }}</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="user" class="table table-hover align-middle mb-0">
                         <thead>
                             <tr>
                                 <th>Image</th>
                                 <th>Name</th>
-                                <th>phone</th>
-                                <th>country</th>
-                                <th>balance</th>
+                                <th>Phone</th>
+                                <th>Country</th>
+                                <th>Balance</th>
                                 <th>Status</th>
-                                <th>Action</th>
+                                <th class="text-center">Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($lists as $list)
-                            @php
-                            if (preg_match('/^data:image\/(\w+);base64,/', @$list->image)) {
-                            $user_image = @$list->image;
-                            } else {
-                            $user_image = asset(@$list->image);
-                            }
-                            @endphp
-                            <tr>
-
-                                <td><img src="{{ $user_image }}" style="    width: 50px;height: 50px;"></td>
-                                <td>{!! @$list->name !!}</td>
-                                <td>{{ @$list->phone }} </td>
-                                <td>{{ @$list->country->name }} </td>
-                                <td>{{ currency(@$list->balance) }} </td>
-                                <td>{!! @$list->status() !!}</td>
-                                <td class="text-center">
-                                    <a class="btn btn-small btn-primary mb-2" href="{{ route('admin.users.show', $list->id) }}">
-                                        <i class="fa fa-eye"></i> View More
-                                    </a>
-                                </td>
-                            </tr>
+                                @php
+                                    if (preg_match('/^data:image\/(\w+);base64,/', @$list->image)) {
+                                        $user_image = @$list->image;
+                                    } else {
+                                        $user_image = asset(@$list->image);
+                                    }
+                                @endphp
+                                <tr>
+                                    <td><img src="{{ $user_image }}" class="user-img"></td>
+                                    <td class="fw-semibold">{!! @$list->name !!}</td>
+                                    <td>{{ @$list->phone }}</td>
+                                    <td>{{ @$list->country->name }}</td>
+                                    <td>{{ currency(@$list->balance) }}</td>
+                                    <td>{!! @$list->status() !!}</td>
+                                    <td class="text-center">
+                                        <a class="btn btn-sm btn-outline-primary" href="{{ route('admin.users.show', $list->id) }}">
+                                            <i class="fa fa-eye"></i> View
+                                        </a>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </div>
+
+                <!-- Laravel pagination -->
+                <div class="mt-3 d-flex justify-content-center">
+                    {{ $lists->links('pagination::bootstrap-5') }}
+                </div>
             </div>
         </div>
-
     </div>
 </div>
-<div class="mt-2 mb-5" >
-    {{ $lists->links('pagination::bootstrap-5') }}
-</div>
-
-
 @endsection
+
 @section('script')
-
-
+<!-- DataTables with Bootstrap 5 -->
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
 <script type="text/javascript">
-    let table = new DataTable('#user');
+    let table = new DataTable('#user', {
+        paging: false, // Laravel handles pagination
+        info: false,
+        searching: true
+    });
 </script>
-
 @endsection
